cd2ed89ddec583ada1fb9a1b5ddde80dd40d25b7
lucascolley <lucas.colley8@gmail.com>
2021-10-01 14:09:00 +0100

    fixed user changing password

--- cover/change_pwd.php
+++ cover/change_pwd.php
@@ -7,7 +7,7 @@
       if (isset($_SESSION["userEmail"])) {
           echo "<h1>Change Password!</h1>";
           echo "<form action='includes/change_pwd.inc.php' method='post'>";
-          echo " <input type='hidden' name='email' value='" . $_SESSION['email'] . "'>";
+          echo " <input type='hidden' name='email' value='". $_SESSION['userEmail'] ."'>";
           echo " <input type='password' name='pwd' placeholder='current password...'>";
           echo " <input type='password' name='newpwd' placeholder='new password...'>";
           echo " <input type='password' name='newpwdrepeat' placeholder='repeat new password...'>";
