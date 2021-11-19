a98901dcdcab9228bac92ca01a039028743df49a
lucascolley <lucas.colley8@gmail.com>
2021-11-19 09:27:40 +0000

    added script to anonymise staff from SIMS data

---
+++ anonymise/anonymise.py
@@ -0,0 +1,19 @@
+def anonymise(timetable, names):
+    for i in range(96):
+        timetable[21 * i + 1] = "Timetable - " + names[i]
+    return timetable
+
+
+def main():
+    with open("staffTT.csv") as file:
+        timetable = file.readlines()
+    with open("randomNamesList.txt") as file:
+        names = file.readlines()
+    timetable = anonymise(timetable, names)
+    with open("staffTTanon.csv", "a+") as file:
+        for line in timetable:
+            file.write(line)
+
+
+if __name__ == '__main__':
+    main()
