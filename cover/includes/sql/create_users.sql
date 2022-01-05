687179a7db28e1ff667a07091b5b1ae9a108d309
lucascolley <lucas.colley8@gmail.com>
2022-01-05 15:47:28 +0000

    updated create users sql

--- cover/includes/sql/create_users.sql
+++ cover/includes/sql/create_users.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users (
-    usersEmail varchar(255) PRIMARY KEY NOT NULL,
-    usersPwd varchar(64) DEFAULT "$2y$10$ahWSLqMBx0Z.trlh6x3x9ejA8vSrdGiVB72VVcSmjgVE2ui5ejA7u",
-    usersTitle varchar(4),
-    usersForename varchar(255),
-    usersSurname varchar(255),
-    usersStaffCode varchar(3),
-    usersAdmin tinyint(1) DEFAULT 0,
-    usersTeacher tinyint(1) DEFAULT 1
+    usersEmail VARCHAR(255) PRIMARY KEY NOT NULL,
+    usersPwd VARCHAR(64) DEFAULT "$2y$10$ahWSLqMBx0Z.trlh6x3x9ejA8vSrdGiVB72VVcSmjgVE2ui5ejA7u",
+    usersTitle VARCHAR(4),
+    usersForename VARCHAR(255),
+    usersSurname VARCHAR(255),
+    usersStaffCode VARCHAR(3),
+    usersAdmin TINYINT(1) DEFAULT 0,
+    usersTeacher TINYINT(1) DEFAULT 1
 );
