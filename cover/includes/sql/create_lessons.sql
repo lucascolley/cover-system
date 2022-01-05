1be4b1c1fbea35aca93099584cdf19329aab64dd
lucascolley <lucas.colley8@gmail.com>
2022-01-05 15:46:54 +0000

    create lessons sql added

---
+++ cover/includes/sql/create_lessons.sql
@@ -0,0 +1,9 @@
+CREATE TABLE lessons (
+    lessonID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    teacherEmail VARCHAR(255) NOT NULL,
+    classCode VARCHAR(7),
+    week TINYINT(1),
+    day VARCHAR(3),
+    period TINYINT(1),
+    room VARCHAR(3)
+);
