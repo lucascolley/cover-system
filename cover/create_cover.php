1b187ad5c122c736f26f3d56682b916c763b143c
lucascolley <lucas.colley8@gmail.com>
2022-03-03 10:23:51 +0000

    started create_cover

--- cover/create_cover.php
+++ cover/create_cover.php
@@ -1,16 +1,20 @@
 <?php
   include_once 'header.php'
 ?>
-      <section class="cover-form">
+      <section class="create_cover-form">
         <?php
         if (isset($_SESSION["userEmail"])) {
-            if ($_SESSION["admin"] !== 0) { ?>
-              
-              <?php
+            if ($_SESSION["admin"] !== 0) {
               if (isset($_GET["error"])) {
                   if ($_GET["error"] == "emptyinput") {
                       echo "<p>Please fill in all fields!</p>";
                   }
+              }
+              if (isset($_GET["date"])) {
+                $date = $_GET["date"];
+                
+              } else {
+                header("location: ./cover.php");
               }
             } else {
                 header("location: ./index.php");
