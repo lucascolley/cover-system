6964fb31a7c7253d658e01df0d44d167dd0c6169
lucascolley <lucas.colley8@gmail.com>
2022-03-12 21:12:36 +0000

    current date prefilled in cover.php

--- cover/cover.php
+++ cover/cover.php
@@ -10,7 +10,7 @@
                 <meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <title>jQuery UI Datepicker - Default functionality</title>
-                <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+                <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
                 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
                 <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
                 <script>
@@ -21,7 +21,8 @@
               </head>
               <body>
                 <form action='includes/cover.inc.php' method='post'>
-                  Enter Date: <input type='text' name='date' id='datepicker'><br>
+                  Enter Date: <input type='text' name='date' id='datepicker'
+                                     value=<?php echo "'" . date('d-m-Y') . "'"?>><br>
                   <button type='submit' name='submit'>Select Cover Lessons</button>
                 </form>
               </body>
