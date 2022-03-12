e67d68cc09e77d14dd5f21c0df36521cc56c1925
lucascolley <lucas.colley8@gmail.com>
2022-03-12 19:08:41 +0000

    implemented getTeachers into create_cover.php

--- cover/create_cover.php
+++ cover/create_cover.php
@@ -12,26 +12,36 @@
               }
               if (isset($_GET["date"])) {
                 $date = $_GET["date"];
+                require_once 'includes/dbh.inc.php';
+                require_once 'includes/functions.inc.php';
+                $teachers = getTeachers($conn);
                 ?>
                 <h1>Create Cover for Day</h1>
                 <head>
                   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
                   <script>
                   $(function() {
-                      $( "#but1" ).click(function()
+                    var teachers = <?php echo json_encode($teachers); ?>;
+                    $.each(teachers, function (i, teacher) {
+                        var entry = teacher[0] + ' ' + teacher[1] + ' ' + teacher[2] + ' ' + teacher[3];
+                        $("#listbox1").append('<option>' + entry + '</option>');
+                    });
+                  });
+                  $(function() {
+                    $( "#but1" ).click(function()
+                    {
+                    $("#listbox1 option:selected").each(function()
                       {
-                      $("#listbox1 option:selected").each(function()
-                        {
-                          $(this).remove().appendTo("#listbox2");
-                          $("#listbox2").append($("#listbox2 option")
-                                            .remove().sort(function(a, b) {
-                              var at = $(a).text(),
-                                  bt = $(b).text();
-                              return (at > bt) ? 1 : ((at < bt) ? -1 : 0);
-                          }));
-                        });
+                        $(this).remove().appendTo("#listbox2");
+                        $("#listbox2").append($("#listbox2 option")
+                                          .remove().sort(function(a, b) {
+                            var at = $(a).text(),
+                                bt = $(b).text();
+                            return (at > bt) ? 1 : ((at < bt) ? -1 : 0);
+                        }));
                       });
                     });
+                  });
                   $(function() {
                       $( "#but2" ).click(function()
                       {
@@ -52,18 +62,12 @@
                 <table border="1">
                   <tr>
                     <th>Present Teachers</th>
-                    <th>Transfer Values</th>
+                    <th>Transfer Selection(s)</th>
                     <th>Absent Teachers</th>
                   </tr>
                   <tr>
                     <td>
-                      <select id="listbox1" multiple="multiple" size="10">
-                        <option>tchra</option>
-                        <option>tchrb</option>
-                        <option>tchrc</option>
-                        <option>tchrd</option>
-                        <option>tchre</option>
-                      </select>
+                      <select id="listbox1" multiple="multiple" size="10"></select>
                     </td>
                     <td>
                       <input type="button" id="but1" value=">>"/>
