a215f9e68120403f6e005fee5eca3f5b0df2c54a
lucascolley <lucas.colley8@gmail.com>
2022-03-15 12:14:03 +0000

    made usersEmail case sensitive

--- cover/includes/sql/create_users.sql
+++ cover/includes/sql/create_users.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (usersEmail VARCHAR(255) PRIMARY KEY NOT NULL,
+CREATE TABLE users (usersEmail VARCHAR(255) PRIMARY KEY CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
 usersPwd VARCHAR(64) DEFAULT "$2y$10$ahWSLqMBx0Z.trlh6x3x9ejA8vSrdGiVB72VVcSmjgVE2ui5ejA7u",
 usersTitle VARCHAR(4),
 usersForename VARCHAR(255),
