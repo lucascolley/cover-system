ea2086bf4f52d7e55bf4512c742d7102baec2957
lucascolley <lucas.colley8@gmail.com>
2022-03-22 10:34:58 +0000

    removed unnecessary error handling

--- cover/create_cover.php
+++ cover/create_cover.php
@@ -6,11 +6,6 @@
         // Server-side scripting using request and response objects
         if (isset($_SESSION["userEmail"])) {
             if ($_SESSION["admin"] !== 0) {
-                if (isset($_GET["error"])) {
-                    if ($_GET["error"] == "emptyinput") {
-                        echo "<p>Please fill in all fields!</p>";
-                    }
-                }
                 if (isset($_GET["date"])) {
                     $date = $_GET["date"];
                     require_once 'includes/dbh.inc.php';
