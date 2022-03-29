6bad8c1b4b68bc89c7e879705e15c59ccb124a6e
lucascolley <lucas.colley8@gmail.com>
2022-03-29 12:09:20 +0100

    started select_periods.inc.php

---
+++ cover/includes/select_periods.inc.php
@@ -0,0 +1,22 @@
+<?php
+  
+  // if (isset($_POST["submit"])) {
+  //     $date = $_POST["date"];
+  //     $absentTeachers = array();
+  //     foreach ($_POST['selectName'] as $teacher) {
+  //         $absentTeachers[] = $teacher;
+  //     }
+  //
+  //     // update absences table with staff codes for chosen date
+  //     require_once 'dbh.inc.php';
+  //     require_once 'functions.inc.php';
+  //
+  //     updateAbsences($conn, $date, $absentTeachers);
+  //
+  //     // pass date through to select_periods
+  //     header("location: ../select_periods.php?date=" . $date);
+  //     exit();
+  // } else {
+  //     header("location: ../create_cover.php");
+  //     exit();
+  // }
