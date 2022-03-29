f3916d79903a8a8d100ea1193dd071727ff5d8c5
lucascolley <lucas.colley8@gmail.com>
2022-03-29 09:49:20 +0100

    select periods now working with table display for absent periods

--- cover/select_periods.php
+++ cover/select_periods.php
@@ -6,90 +6,42 @@
         if (isset($_SESSION["userEmail"])) {
             if ($_SESSION["admin"] !== 0) {
                 if (isset($_GET["date"])) {
-                    $date = $_GET["date"];
-                    ?>
+                    $date = $_GET["date"]; ?>
                 <h1>Tick Boxes for Absent Periods</h1>
                 <head>
                 </head>
                 <?php
                 // get array of absent teachers
                 require_once 'includes/dbh.inc.php';
-                require_once 'includes/functions.inc.php';
-                $absentTeachers = getAbsences($conn, $date);
-                echo "<pre>";
-                print_r($absentTeachers);
-                echo "</pre>";
+                    require_once 'includes/functions.inc.php';
+                    $absentTeachers = getAbsences($conn, $date);
 
-                $cols = 7;
+                    $output = "<form action='includes/select_periods.inc.php' method='post'>";
+                    $output .= "<table border='1'>\n<tr>\n<th>Absent Teachers</th>\n";
+                    $output .= "<th>P1</th>\n<th>P2</th>\n<th>P3</th>\n<th>P4</th>\n";
+                    $output .= "<th>P5</th>\n<th>P6</th>\n</tr>";
 
-                $output = "<form action='includes/select_periods.inc.php' method='post'>";
-                $output .= "<table border='1'>\n<tr>\n<th>Absent Teachers</th>\n";
-                $output .= "<th>P1</th>\n<th>P2</th>\n<th>P3</th>\n<th>P4</th>\n";
-                $output .= "<th>P5</th>\n<th>P6</th>\n</tr>";
-
-                $period_count = 0;
-
-                for ($i = 0; $i < count($absentTeachers); $i++) {
-                    $teacherName = $absentTeachers[i][0];
-                    if ($period_count == 0) {
+                    for ($teacher_count = 0; $teacher_count < count($absentTeachers); $teacher_count++) {
+                        $teacherName = $absentTeachers[$teacher_count][0];
                         $output .= "<tr>\n<td>" . $teacherName . "</td>\n";
-                    }
-                    else {
-                      $period = "p" . $period_count;
-                      // pre-check boxes where absent in database
-                      $checked = " checked";
-                      $output .= "<input type='checkbox' id=" . $period . " name= " . $period . " value=" . $period . ">\n";
-                    }
-
-                    $period_count++;
-
-                    // end the row if we've generated the expected number of columns
-                    // or if we're at the end of the array
-                    if ($period_count == $cols || $i == (count($absentTeachers) - 1)) {
+                        for ($period_count = 1; $period_count < 7; $period_count++) {
+                            $period = "p" . $period_count;
+                            // pre-check boxes where absent in database
+                            if ($absentTeachers[$teacher_count][$period_count] == 1) {
+                                $checked = " checked";
+                            } else {
+                                $checked = "";
+                            }
+                            $output .= "<td>\n<input type='checkbox' id=" . $period;
+                            $output .= " name= " . $period . " value=" . $period;
+                            $output .= $checked . ">\n</td>\n";
+                        }
                         $output .= "</tr>\n";
                         $period_count = 0;
                     }
-                }
 
-                $output .= "</table>\n</form>\n";
-                echo $output
-                ?>
-                <form action="includes/select_periods.inc.php" method="post">
-                  <table border="1">
-                    <tr>
-                      <th>Absent Teachers</th>
-                      <th>P1</th>
-                      <th>P2</th>
-                      <th>P3</th>
-                      <th>P4</th>
-                      <th>P5</th>
-                      <th>P6</th>
-                    </tr>
-                    <tr>
-                      <td>
-                        <!--teacher name-->
-                      </td>
-                      <td>
-                        <input type="checkbox" id="p1" name="p1" value="p1">
-                      </td>
-                      <td>
-                        <input type="checkbox" id="p2" name="p2" value="p2">
-                      </td>
-                      <td>
-                        <input type="checkbox" id="p3" name="p3" value="p3">
-                      </td>
-                      <td>
-                        <input type="checkbox" id="p4" name="p4" value="p4">
-                      </td>
-                      <td>
-                        <input type="checkbox" id="p5" name="p5" value="p5">
-                      </td>
-                      <td>
-                        <input type="checkbox" id="p6" name="p6" value="p6">
-                      </td>
-                    </tr>
-                  </table>
-                </form>
+                    $output .= "</table>\n</form>\n";
+                    echo $output; ?>
                 <?php
                 } else {
                     header("location: ./cover.php");
