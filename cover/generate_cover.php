1370d85f040b34c08384a7504eaa9d7009aead13
lucascolley <lucas.colley8@gmail.com>
2022-04-06 16:04:19 +0100

    started generate_cover.php

---
+++ cover/generate_cover.php
@@ -0,0 +1,31 @@
+<?php
+  include_once 'header.php'
+?>
+      <section>
+        <?php
+        if (isset($_SESSION["userEmail"])) {
+            if ($_SESSION["admin"] !== 0) {
+                if (isset($_GET["date"])) {
+                    $date = $_GET["date"]; ?>
+                <h1></h1>
+                <head>
+                </head>
+                <?php
+                } else {
+                    header("location: ./cover.php");
+                }
+            } else {
+                header("location: ./index.php");
+                exit();
+            }
+        } else {
+            header("location: ./login.php");
+            exit();
+        }
+        ?>
+      </section>
+
+
+<?php
+  include_once 'footer.php'
+?>
