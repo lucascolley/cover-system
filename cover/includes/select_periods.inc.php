6fceb00e2eb2009e53decb1a35635a3e62a2fdd9
lucascolley <lucas.colley8@gmail.com>
2022-04-06 09:50:07 +0100

    periods now get passed through to select_periods.inc.php correctly

--- cover/includes/select_periods.inc.php
+++ cover/includes/select_periods.inc.php
@@ -1,24 +1,38 @@
 <?php
-  // // get array of absent teachers and periods from post
-  // // update the absences table
-  // // pass date through
-  // if (isset($_POST["submit"])) {
-  //     $date = $_POST["date"];
-  //     $absentTeachers = array();
-  //     foreach ($_POST['selectName'] as $teacher) {
-  //         $absentTeachers[] = $teacher;
-  //     }
-  //
-  //     // update absences table with staff codes for chosen date
-  //     require_once 'dbh.inc.php';
-  //     require_once 'functions.inc.php';
-  //
-  //     updateAbsences($conn, $date, $absentTeachers);
-  //
-  //     // pass date through to select_periods
-  //     header("location: ../select_periods.php?date=" . $date);
-  //     exit();
-  // } else {
-  //     header("location: ../create_cover.php");
-  //     exit();
-  // }
+  if (isset($_POST["submit"])) {
+      $date = $_POST["date"];
+      echo $date;
+
+      require_once 'dbh.inc.php';
+      require_once 'functions.inc.php';
+      $absentTeachers = getAbsences($conn, $date);
+
+      for ($teacher_count = 0; $teacher_count < count($absentTeachers); $teacher_count++) {
+          for ($period_count = 1; $period_count < 7; $period_count++) {
+              $period = $teacher_count . "p" . $period_count;
+              if (isset($_POST[$period])) {
+                $absentTeachers[$teacher_count][$period_count] = 1;
+              }
+              else {
+                $absentTeachers[$teacher_count][$period_count] = 0;
+              }
+          }
+      }
+
+      echo "<pre>";
+      print_r($absentTeachers);
+      echo "</pre>";
+
+      // // update absences table
+      // require_once 'dbh.inc.php';
+      // require_once 'functions.inc.php';
+      //
+      // // updateAbsences($conn, $date, $absentTeachers);
+      //
+      // // pass date through
+      // header("location: ../select_periods.php?date=" . $date);
+      // exit();
+  } else {
+      header("location: ../create_cover.php");
+      exit();
+  }
--- cover/select_periods.php
+++ cover/select_periods.php
@@ -16,8 +16,9 @@
                     require_once 'includes/functions.inc.php';
                     $absentTeachers = getAbsences($conn, $date);
 
-                    $output = "<form action='includes/select_periods.inc.php' method='post'>";
-                    $ouput .= "<input type='hidden' id='date' name='date' value='" . $date . "'>";
+                    $output = "\n<form action='includes/select_periods.inc.php' method='post'>\n";
+                    $output .= "<input type='hidden' id='date' name='date' value='" . $date . "'>\n";
+                    $output .= "<input type='hidden' id='teachers' name='teachers' value='" . $absentTeachers . "'>\n";
                     $output .= "<table border='1'>\n<tr>\n<th>Absent Teachers</th>\n";
                     $output .= "<th>P1</th>\n<th>P2</th>\n<th>P3</th>\n<th>P4</th>\n";
                     $output .= "<th>P5</th>\n<th>P6</th>\n</tr>";
@@ -26,7 +27,7 @@
                         $teacherName = $absentTeachers[$teacher_count][0];
                         $output .= "<tr>\n<td>" . $teacherName . "</td>\n";
                         for ($period_count = 1; $period_count < 7; $period_count++) {
-                            $period = "p" . $period_count;
+                            $period = $teacher_count . "p" . $period_count;
                             // pre-check boxes where absent in database
                             if ($absentTeachers[$teacher_count][$period_count] == 1) {
                                 $checked = " checked";
@@ -34,7 +35,7 @@
                                 $checked = "";
                             }
                             $output .= "<td>\n<input type='checkbox' id=" . $period;
-                            $output .= " name= " . $period . " value=" . $period;
+                            $output .= " name=" . $period . " value=" . $period;
                             $output .= $checked . ">\n</td>\n";
                         }
                         $output .= "</tr>\n";
@@ -43,7 +44,7 @@
 
                     $output .= "</table>\n";
                     $output .= "<button type='submit' name='submit' value=Submit>Submit</button>";
-                    $output .= "</form>\n";
+                    $output .= "\n</form>\n";
 
                     echo $output; ?>
                 <?php
