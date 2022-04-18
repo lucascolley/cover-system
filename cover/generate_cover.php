66afcce91694bbc8f669c10e01f9139be8a80626
lucascolley <lucas.colley8@gmail.com>
2022-04-18 17:34:45 +0100

    display cover output in table

--- cover/generate_cover.php
+++ cover/generate_cover.php
@@ -11,10 +11,32 @@
                 </head>
                 <?php
                 $matches = unserialize($_POST["matches"]);
+                $keys = array_column($matches, 'period');
+                array_multisort($keys, SORT_ASC, $matches);
                 // output matching results
-                echo "<pre>"; //
-                print_r($matches); //
-                echo "</pre>"; //
+                // echo "<pre>"; //
+                // print_r($matches); //
+                // echo "</pre>"; //
+                ?>
+                <br />
+                <table>
+                  <tr>
+                    <?php
+                    foreach($matches[0] as $heading => $cell) {
+                        echo('<th>' . $heading . '</th>');
+                    }
+                    ?>
+                  </tr>
+
+                  <?php foreach($matches as $match) {
+                    echo('<tr>');
+                    foreach($match as $heading => $cell) {
+                      echo('<td>' . $cell . '</td>');
+                    }
+                    echo('</tr>');
+                  } ?>
+                </table>
+                <?php
                 } else {
                     header("location: ./cover.php");
                 }
