e174037ebd1e15b9c011cb55eb8750197498e234
lucascolley <lucas.colley8@gmail.com>
2022-04-19 14:50:57 +0100

    formatting

--- cover/generate_cover.php
+++ cover/generate_cover.php
@@ -6,65 +6,58 @@
         if (isset($_SESSION["userEmail"])) {
             if ($_SESSION["admin"] !== 0) {
                 if (isset($_POST["matches"])) {
-                  $date = $_POST["date"]; ?>
-                  <br />
-                  <h1>Review Suggested Cover Teachers:</h1>
-                  <br />
-                  <h3>You can override the generated choices here.</h3>
-                  <br />
-                  <h3>Please note: Ensure there are no errors if you choose
-                  to override choices as this is the final confirmation.</h3>
-                  <head>
-                  </head>
-                  <?php
-                  $matchesString = $_POST["matches"];
-                  $matches = unserialize($matchesString);
-                  $keys = array_column($matches, 'period');
-                  array_multisort($keys, SORT_ASC, $matches);
-                  // output matching results
-                  // echo "<pre>"; //
-                  // print_r($matches); //
-                  // echo "</pre>"; //
-                  ?>
-                  <br />
-                  <form action='includes/generate_cover.inc.php' method='post'>
-                    <input type="hidden" id="date" name="date" value=<?php echo('"' . $date . '"');?>>
+                    $date = $_POST["date"]; ?>
+                    <br />
+                    <h1>Review Suggested Cover Teachers:</h1>
+                    <br />
+                    <h3>You can override the generated choices here.</h3>
+                    <br />
+                    <h3>Please note: Ensure there are no errors if you choose
+                    to override choices as this is the final confirmation.</h3>
+                    <head>
+                    </head>
                     <?php
-                    echo "<input type='hidden' id='matches' name='matches' value='" . $matchesString . "'/>"
-                    ?>
-                    <table>
-                      <tr>
-                        <?php
-                        foreach($matches[0] as $heading => $cell) {
-                            echo('<th>' . $heading . '</th>');
-                        }
-                        ?>
-                      </tr>
-
+                    $matchesString = $_POST["matches"];
+                    $matches = unserialize($matchesString);
+                    $keys = array_column($matches, 'period');
+                    array_multisort($keys, SORT_ASC, $matches); ?>
+                    <br />
+                    <form action='includes/generate_cover.inc.php' method='post'>
+                      <input type="hidden" id="date" name="date" value=<?php echo('"' . $date . '"'); ?>>
                       <?php
-                      $i = 0;
-                      foreach($matches as $match) {
-                          echo('<tr>');
-                          $j = 0;
-                          foreach($match as $heading => $cell) {
-                              if ($j == 0) {
-                                  // input with default value as generated
-                                  // optional override
-                                  $output = "<td><input type='text' id='match";
-                                  $output .= ($i . "' name='match" . $i);
-                                  $output .= "' value='" . $cell . "' /></td>";
-                                  echo $output;
-                              } else {
-                                  echo('<td>' . $cell . '</td>');
-                              }
-                              $j++;
-                          }
-                          echo('</tr>');
-                          $i++;
-                      } ?>
-                    </table>
-                    <button type='submit' name='submit' value=Submit>Confirm Cover</button>
-                  </form>
+                      echo "<input type='hidden' id='matches' name='matches' value='" . $matchesString . "'/>"
+                      ?>
+                      <table>
+                        <tr>
+                          <?php
+                          foreach ($matches[0] as $heading => $cell) {
+                              echo('<th>' . $heading . '</th>');
+                          } ?>
+                        </tr>
+
+                        <?php
+                        $i = 0;
+                    foreach ($matches as $match) {
+                        echo('<tr>');
+                        $j = 0;
+                        foreach ($match as $heading => $cell) {
+                            if ($j == 0) {
+                                // input with default value as generated
+                                $output = "<td><input type='text' id='match";
+                                $output .= ($i . "' name='match" . $i);
+                                $output .= "' value='" . $cell . "' /></td>";
+                                echo $output;
+                            } else {
+                                echo('<td>' . $cell . '</td>');
+                            }
+                            $j++;
+                        }
+                        echo('</tr>');
+                        $i++;
+                    } ?>
+                      </table>
+                      <button type='submit' name='submit' value=Submit>Confirm Cover</button>
+                    </form>
                 <?php
                 } else {
                     header("location: ./cover.php");
--- cover/includes/generate_cover.inc.php
+++ cover/includes/generate_cover.inc.php
@@ -1,12 +1,15 @@
 <?php
+
   if (isset($_POST["submit"])) {
       $date = $_POST["date"];
       $matchesString = $_POST["matches"];
       $matches = unserialize($matchesString);
+      $keys = array_column($matches, 'period');
+      array_multisort($keys, SORT_ASC, $matches);
       $i = 0;
-      foreach($matches as $match) {
+      foreach ($matches as $match) {
           $j = 0;
-          foreach($match as $heading => $cell) {
+          foreach ($match as $heading => $cell) {
               if ($j == 0) {
                   $id = "match" . $i;
                   $matches[$i]['coverStaffCode'] = $_POST[$id];
@@ -28,7 +31,7 @@
 
       // pass through to next page
       // header("location: ../select_periods.php?date=" . $date);
-      exit();
+      // exit();
   } else {
       header("location: ../cover.php");
       exit();
