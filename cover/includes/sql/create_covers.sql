a1a10c7e892edff07afc5e946a790039cf6eef08
lucascolley <lucas.colley8@gmail.com>
2022-04-19 17:25:14 +0100

    updated covers table sql

--- cover/includes/sql/create_covers.sql
+++ cover/includes/sql/create_covers.sql
@@ -1,4 +1,8 @@
 CREATE TABLE covers (coverDate date NOT NULL,
 lessonID INT NOT NULL,
 coverStaffCode VARCHAR(3) NOT NULL,
+period TINYINT(1) NOT NULL,
+room VARCHAR(3) NOT NULL,
+staffCode VARCHAR(3) NOT NULL,
+classCode VARCHAR(7),
 PRIMARY KEY (coverDate, lessonID));
