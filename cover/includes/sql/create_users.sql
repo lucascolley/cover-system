63e113a99db94013f92c5ebc24bec45b47de86d7
lucascolley <lucas.colley8@gmail.com>
2022-04-20 16:54:57 +0100

    users sql fixed

--- cover/includes/sql/create_users.sql
+++ cover/includes/sql/create_users.sql
@@ -1,8 +1,11 @@
-CREATE TABLE users (usersEmail VARCHAR(255) PRIMARY KEY CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+CREATE TABLE users (
+usersEmail VARCHAR(255) PRIMARY KEY NOT NULL,
 usersPwd VARCHAR(64) DEFAULT "$2y$10$ahWSLqMBx0Z.trlh6x3x9ejA8vSrdGiVB72VVcSmjgVE2ui5ejA7u",
 usersTitle VARCHAR(4),
 usersForename VARCHAR(255),
 usersSurname VARCHAR(255),
 usersStaffCode VARCHAR(3),
 usersAdmin TINYINT(1) DEFAULT 0,
-usersSLT TINYINT(1) DEFAULT 0);
+usersTeacher TINYINT(1) DEFAULT 1,
+usersSLT TINYINT(1) DEFAULT 0)
+CHARACTER SET latin1 COLLATE latin1_general_cs;
--- cover/includes/sql/insert_users_tests.sql
+++ cover/includes/sql/insert_users_tests.sql
@@ -1,3 +1,3 @@
-INSERT INTO `users`(`usersEmail`, `usersPwd`, `usersAdmin`)
-VALUES ('a','$2y$10$n2diM6jtnp7UU.Icn2b5Lu0aNZZ6dX255MRgCP3mdelLPsGjF0.ue','1'),
-       ('b','$2y$10$BS6zw9OwnLEiWfrDOrr.a.fKR62hzOR.mzvLVfeODyDqlunEp8WXm','0');
+INSERT INTO `users`(`usersEmail`, `usersPwd`, `usersAdmin`, `usersTeacher`)
+VALUES ('a','$2y$10$n2diM6jtnp7UU.Icn2b5Lu0aNZZ6dX255MRgCP3mdelLPsGjF0.ue','1', '0'),
+       ('b','$2y$10$BS6zw9OwnLEiWfrDOrr.a.fKR62hzOR.mzvLVfeODyDqlunEp8WXm','0', '0');
