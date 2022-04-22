e4c714aa8798d0d7a5d51d6715e641793a56888a
lucascolley <lucas.colley8@gmail.com>
2022-04-22 17:15:04 +0100

    put admin settings buttons in a a table

--- cover/admin_settings.php
+++ cover/admin_settings.php
@@ -4,10 +4,14 @@
       <section>
         <?php
         if (isset($_SESSION["userEmail"])) {
-            echo "<a href='create_user.php' class='button'>Create User</a>";
-            echo "<a href='delete_user.php' class='button'>Delete User</a>";
-            echo "<a href='import_teachers.php' class='button'>Import Teachers</a>";
-            echo "<a href='set_SLT.php' class='button'>Set SLT Teachers</a>";
+            ?>
+            <table>
+              <td><a href='create_user.php' class='button'>Create User</a></td>
+              <td><a href='delete_user.php' class='button'>Delete User</a></td>
+              <td><a href='import_teachers.php' class='button'>Import Teachers</a></td>
+              <td><a href='set_SLT.php' class='button'>Set SLT Teachers</a></td>
+            </table>
+            <?php
         } else {
             header("location: ./login.php");
             exit();
