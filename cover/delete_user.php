64b0859fafd72d69387a6fd2b51a1433cfc3a00a
lucascolley <lucas.colley8@gmail.com>
2022-04-22 19:47:30 +0100

    added some line breaks and some text to import teachers

--- cover/delete_user.php
+++ cover/delete_user.php
@@ -2,7 +2,9 @@
   include_once 'header.php'
 ?>
       <section class="delete_user-form">
+        <br>
         <h1>Delete a User</h1>
+        <br>
         <form action="includes/delete_user.inc.php" method="post">
           <input type="text" name="userEmail" placeholder="user email...">
           <?php
@@ -11,6 +13,7 @@
           <input type="password" name="pwd" placeholder="admin password...">
           <button type="submit" name="submit">Delete User</button>
         </form>
+        <br>
         <?php
         if (isset($_GET["error"])) {
             if ($_GET["error"] == "emptyinput") {
--- cover/import_teachers.php
+++ cover/import_teachers.php
@@ -2,10 +2,15 @@
   include_once 'header.php'
 ?>
       <section class="import_teachers-form">
+        <br>
         <h1>Import Teachers</h1>
+        <br>
+        <h3>Place csv at 'cover/includes/import/users.csv' and hit go!</h3>
+        <br>
         <form action="includes/import_teachers.inc.php" method="post">
-          <button type="submit" name="submit">Import Teachers</button>
+          <button type="submit" name="submit">Go!</button>
         </form>
+        <br>
         <?php
         if (isset($_GET["error"])) {
             if ($_GET["error"] == "stmtfailed") {
