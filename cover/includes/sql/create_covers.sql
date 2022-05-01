14ea6294bd2b748750c4db01e12297831dc0d56c
lucascolley <lucas.colley8@gmail.com>
2022-05-01 11:05:44 +0100

    removed NOT NULL from room in covers DDL

--- cover/includes/sql/create_covers.sql
+++ cover/includes/sql/create_covers.sql
@@ -2,7 +2,7 @@ CREATE TABLE covers (coverDate date NOT NULL,
 lessonID INT NOT NULL,
 coverStaffCode VARCHAR(3) NOT NULL,
 period TINYINT(1) NOT NULL,
-room VARCHAR(3) NOT NULL,
+room VARCHAR(3),
 staffCode VARCHAR(3) NOT NULL,
 classCode VARCHAR(10),
 PRIMARY KEY (coverDate, lessonID));
